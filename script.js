51df821eda7efef0fedd6a3b76ea0b10be74824c
Raynos <raynos2@gmail.com>
2013-05-31 11:40:11 -0700

    updated script

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ var NODE_ENV = require("./index")
 module.exports = envScript
 
 function envScript() {
-    return "window.NODE_ENV = " + JSON.stringify(NODE_ENV)
+    return ";(function () {\n" +
+        "    window.NODE_ENV = '" + JSON.stringify(NODE_ENV) + "'\n" +
+        "}());\n"
 }
